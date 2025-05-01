b08c25f52c0029d9a7ea072de1496b406675ebac
Josua Friederichs <josua.friederichs@outlook.de>
2025-05-01 13:33:23 +0200

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Josua Friederichs on 01.05.25.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
