109996be85ee8015c7f36b626803a593a01510e5
Josua Friederichs <josua.friederichs@outlook.de>
2025-05-08 18:25:03 +0200

    Inital Commit

--- Code History/ContentView.swift
+++ Code History/ContentView.swift
@@ -8,17 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let question: Question? = nil
+    private let mainColor = Color(
+        red: 20 / 255,
+        green: 28 / 255,
+        blue: 58 / 255
+    )
+    private let textColor = Color.white
+    private let optionCount = 4
+
+    @State private var openTriviaQuestions: [OpenTriviaQuestion]?
+    private var index: Int = 0
+
+    init(previewQuestions: [OpenTriviaQuestion]? = nil) {
+        self._openTriviaQuestions = State(initialValue: previewQuestions)
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            mainColor.ignoresSafeArea()
+
+            if let questions = openTriviaQuestions {
+                VStack(alignment: .center, spacing: 20) {
+                    Text("\(index + 1) / \(questions.count)")
+                        .font(.callout)
+                        .multilineTextAlignment(.leading)
+                        .padding()
+
+                    QuestionView(
+                        question: Question.fromOpenTriviaQuestion(
+                            questions[index]
+                        )
+                    )
+                }
+            } else {
+                ProgressView("Loading Questions...")
+            }
         }
-        .padding()
+        .foregroundStyle(textColor)
+        .task {
+            do {
+                openTriviaQuestions = try await performApiCall()
+            } catch {
+                print("Error while trying to load questions: \(error)")
+                openTriviaQuestions = nil
+            }
+        }
+    }
+
+    func performApiCall() async throws -> [OpenTriviaQuestion] {
+        let url = "https://opentdb.com/api.php?amount=10"
+        let (data, _) = try await URLSession.shared.data(
+            from: URL(string: url)!
+        )
+        let openTriviaResults = try JSONDecoder().decode(
+            OpenTriviaResults.self,
+            from: data
+        )
+        return openTriviaResults.results
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        let previewQuestion = OpenTriviaQuestion(
+            type: "multiple",
+            difficulty: "easy",
+            category: "History",
+            question: "What was the first computer bug?",
+            correct_answer: "Moth",
+            incorrect_answers: ["Ant", "Beetle", "Fly"]
+        )
+
+        ContentView(previewQuestions: [previewQuestion])
+    }
 }
