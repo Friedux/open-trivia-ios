40ff6abd089bff11784255cc3979a00c414c9827
Josua Friederichs <josua.friederichs@outlook.de>
2025-05-09 10:11:56 +0200

    Initial Commit

---
+++ open-trivia-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  open-trivia-ios
+//
+//  Created by Josua Friederichs on 09.05.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ open-trivia-ios/open_trivia_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  open_trivia_iosApp.swift
+//  open-trivia-ios
+//
+//  Created by Josua Friederichs on 09.05.25.
+//
+
+import SwiftUI
+
+@main
+struct open_trivia_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
